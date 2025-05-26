d7ec1f367cbedadf03387c300d52eb83e46cd94b
MuhAminSyam <aminsyammuh@gmail.com>
2025-05-26 15:03:49 +0700

    pembuatan pewarisan

---
+++ inheritance/inheritance.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "pelajar.h"
+using namespace std;
+
+int main()
+{
+    pelajar siswa1("andi laksono", "SMAN 1 Bantul");
+    cout << siswa1.perkenalan();
+    cout << "Hasil = " << siswa1.jumlah(10, 90) << endl;
+
+    return 0;
+}
